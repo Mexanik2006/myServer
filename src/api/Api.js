aa0ec24b5e7086e846be1ea5a79e051b52bfc114
Mexanik2006 <mexhanik2006@gmail.com>
2024-09-04 13:12:39 +0500

    upload server and download1

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 
-const Api = axios.create({
-    baseURL: 'http://localhost:2600'
-});
 // const Api = axios.create({
-//     baseURL: 'https://portfolionow-back.onrender.com'
+//     baseURL: 'http://localhost:2600'
 // });
+const Api = axios.create({
+    baseURL: 'https://myserverback.onrender.com'
+});
 
 export default Api;
